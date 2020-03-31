26d68611309248e949f6042a0060fc7202cd722d
Yader Coca <ylcoca@gmail.com>
2020-03-31 00:59:47 -0300

    BuildClass Restructure
    Breaking out BuildClass

--- BuisnessLogic/ItemFactory.cs
+++ BuisnessLogic/ItemFactory.cs
@@ -12,37 +12,42 @@ namespace BuisnessLogic
         public static List<ItemObjects> BuildClass(IList<Item> Items)
         {
             List<ItemObjects> itemObjects = new List<ItemObjects>();
-            ItemObjects inheritedItem = null;
+
             foreach (var item in Items)
             {
-                switch (item.Name)
-                {
-                    case "+5 Dexterity Vest":
-                        inheritedItem = new DexterityVest(item);
-                        break;
-                    case "Aged Brie":
-                        inheritedItem = new AgedBrie(item);
-                        break;
-                    case "Elixir of the Mongoose":
-                        inheritedItem = new ElixirOfTheMongoose(item);
-                        break;
-                    case "Sulfuras, Hand of Ragnaros":
-                        inheritedItem = new Sulfuras(item);
-                        break;
-                    case "Backstage passes to a TAFKAL80ETC concert":
-                        inheritedItem = new BackstagePasses(item);
-                        break;
-                    case "Conjured Mana Cake":
-                        inheritedItem = new Conjured(item);
-                        break;
-                }
 
-
-                itemObjects.Add(inheritedItem);
+                itemObjects.Add(ItemObjects(item));
 
             }
 
             return itemObjects;
+        }
+
+        public static ItemObjects ItemObjects(Item item)
+        {
+            ItemObjects inheritedItem = null;
+            switch (item.Name)
+            {
+                case "+5 Dexterity Vest":
+                    inheritedItem = new DexterityVest(item);
+                    break;
+                case "Aged Brie":
+                    inheritedItem = new AgedBrie(item);
+                    break;
+                case "Elixir of the Mongoose":
+                    inheritedItem = new ElixirOfTheMongoose(item);
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    inheritedItem = new Sulfuras(item);
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    inheritedItem = new BackstagePasses(item);
+                    break;
+                case "Conjured Mana Cake":
+                    inheritedItem = new Conjured(item);
+                    break;
+            }
+            return inheritedItem;
         }
     }
 }
